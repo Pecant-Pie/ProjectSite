edf41727aaae7631b63d149a336e85154c14e2c3
Jason <jasonharriot@gmail.com>
2018-09-16 11:31:50 -0400

    misc

--- script.js
+++ script.js
@@ -1,45 +1,10 @@
-for(var i = 30; i >= 0; i--){
-	createSquare(15 * i, i / 20.0, randColor());
-}
+$("body").mousemove(function(event){
+	updateButtons(event);
+});
 
-setInterval(updateSquares, 20);
 
-function createSquare(size, rot, color){
-	var sq = document.createElement("div");
-	sq.style.display = "block";
-	sq.style.position = "absolute";
-	sq.style.backgroundColor = color;
-	sq.style.width = size + "px";
-	sq.style.height = size + "px";
-	sq.style.top = "50%";
-	sq.style.left = "50%";
-	
-	sq.classList.add("square");
-	sq.style.transform = "rotate(0)";
-	
-	sq.setAttribute("data-rot", rot);
-	
-	sq.style.margin = "-" + (size/2) + "px 0px 0px -" + (size/2) + "px";
-	
-	document.getElementsByTagName("body")[0].appendChild(sq);
-}
-
-function updateSquares(){
-	$.each($(".square"), function(index, value){
-		var angle = parseFloat(value.style.transform.substring(7));
-		angle += parseFloat(value.getAttribute("data-rot"));
+function updateButtons(e){
+	for(var i of document.getElementsByClassName("topbarbutton")){
 		
-		if(angle >= 360 || angle <= -360){
-			angle = 0;
-		}
-		value.style.transform = "rotate(" + angle + "deg)";
-	});
-}
-
-function randColor() {
-  var length = 6;
-  var chars = '0123456789ABCDEF';
-  var hex = '#';
-  while(length--) hex += chars[(Math.random() * 16) | 0];
-  return hex;
+	}
 }
