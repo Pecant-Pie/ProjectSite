6b74c48f3d79719e70ca3b0d3e7233cbe61fbf55
Jason <jasonharriot@gmail.com>
2018-11-29 22:32:20 -0500

    Dumped unfinished LeftUbers page

---
+++ leftubers/script.js
@@ -0,0 +1,10 @@
+// $(window).resize(resizeHeaderContainer);
+//
+// function resizeHeaderContainer(){
+// 		var target = $("#headerContainer");
+// 		var parent = target.parent();
+//
+// 		target.css("width", parent.width());
+// }
+//
+// resizeHeaderContainer();
