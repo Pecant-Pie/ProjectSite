03fdf61927557d718ad23de944501eea7ea20cf8
Jason Harriot <jasonharriot@gmail.com>
2018-12-02 22:05:07 -0500

    Colorized, added download badges, search map

--- leftubers/script.js
+++ leftubers/script.js
@@ -1,14 +1,7 @@
-// $(window).resize(resizeHeaderContainer);
-//
-// function resizeHeaderContainer(){
-// 		var target = $("#headerContainer");
-// 		var parent = target.parent();
-//
-// 		target.css("width", parent.width());
-// }
-//
-// resizeHeaderContainer();
-
 $("#backButton").click(function(){
 	window.history.back();
 });
+
+$("#searchButton").click(function(){
+	alert("Invalid ZIP code.");
+})
