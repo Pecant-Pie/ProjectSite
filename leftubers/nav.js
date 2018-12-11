664937bf68e47860906323ec48aeed488ae1c3b0
Jason <jasonharriot@gmail.com>
2018-12-11 13:30:07 -0500

    Leftubers navbar ease in

--- leftubers/nav.js
+++ leftubers/nav.js
@@ -1,12 +1,1 @@
-console.log("nav ok");
-
-$("#home").click(function(){
-	console.log("OK");
-	// window.top.location = "index.html";
-	window.history.back();
-});
-
-$("#alt").click(function(){
-	console.log("OK");
-	window.top.location = "alt.html";
-})
+console.log("nav initialized");
--- leftubers/script.js
+++ leftubers/script.js
@@ -3,9 +3,17 @@ $("#searchButton").click(function(){
 });
 
 $(".downloadBadge").click(function(){
-	alert("No u");
+	alert("Error");
 });
 
 $("#altButton").click(function(){
 	window.top.location = "alt.html";
 });
+
+setTimeout(function(){
+	$("#navContainer").animate({
+		top: "0px"
+	}, 500, function(){
+		console.log("done 1");
+	});
+}, 500);
