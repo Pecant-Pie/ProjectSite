7ec405f17b930f8cd81038f38fce213ff95f57a7
Jason <jasonharriot@gmail.com>
2018-12-11 12:31:57 -0500

    Added dual pages to LeftUbers

--- leftubers/script.js
+++ leftubers/script.js
@@ -1,7 +1,11 @@
-$("#backButton").click(function(){
-	window.history.back();
-});
-
 $("#searchButton").click(function(){
 	alert("Invalid ZIP code.");
-})
+});
+
+$(".downloadBadge").click(function(){
+	alert("No u");
+});
+
+$("#altButton").click(function(){
+	window.top.location = "alt.html";
+});
